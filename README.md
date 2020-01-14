fa06e3bcabae9eb1ccc730aa907a0334ca8fb6d1
hao.liu <l.hao.2012@qq.com>
2020-01-14 13:42:06 +0800

    -init

---
+++ README.md
@@ -0,0 +1,26 @@
+## A simple example
+
+```bootstrap/app.php
+<?php
+
+  ...
+  $app->register(Rainbow2019s\Logger\Providers\LoggerServiceProvider::class);
+  ...
+```  
+
+```php
+<?php
+
+use Laravel\Lumen\Routing\Controller;
+use Illuminate\Http\Request;
+use Rainbow2019s\Logger\Facade\Logger;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+        Logger::info('user add',['id'=>1]);
+        return 'ok';
+    }
+}
+```
---
+++ src/middeleware/BasicHttpAuth.php
@@ -0,0 +1,20 @@
+<?php namespace rainbow2019s\BasicHttpAuth\middleware;
+
+use Closure;
+class BasicHttpAuth{
+  public function handle($request,Closure $next)
+  {
+      $authorization=function($ciphertext,$plaintext) {
+        return substr($ciphertext,6)==base64_encode($palnttext);
+      };
+
+      $auth= $authorization($request->header('authorization'),env('BASiC_HTTP_AUTH'));
+  
+      if(!$auth){
+          $content=['msg'=>'error','dataInfo'=>new \stdClass];
+          return response(json_encode($content),401)->header('Content-type','application/json');
+        };
+
+      return $next($request);
+    }
+}
