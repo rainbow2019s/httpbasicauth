e5e68af706c7c8ecc843ca534d5fb8f25b0ab1d5
hao.liu <l.hao.2012@qq.com>
2020-01-14 21:16:56 +0800

    1.0.4

--- src/middleware/BasicHttpAuth.php
+++ src/middleware/BasicHttpAuth.php
@@ -5,7 +5,7 @@ class BasicHttpAuth{
   public function handle($request,Closure $next)
   {
       $authorization=function($ciphertext,$plaintext) {
-        return substr($ciphertext,6)==base64_encode($palnttext);
+        return substr($ciphertext,6)==base64_encode($plaintext);
       };
 
       $auth= $authorization($request->header('authorization'),env('BASiC_HTTP_AUTH'));
