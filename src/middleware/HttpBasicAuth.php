9cfc20663906d8844827a79d2d082b377317982d
hao.liu <l.hao.2012@qq.com>
2020-01-14 22:40:12 +0800

    1.0.0

--- src/middleware/HttpBasicAuth.php
+++ src/middleware/HttpBasicAuth.php
@@ -1,17 +1,20 @@
-<?php namespace Rainbow2019s\BasicHttpAuth\middleware;
+<?php namespace Rainbow2019s\HttpBasicAuth\middleware;
 
 use Closure;
-class BasicHttpAuth{
+class HttpBasicAuth{
   public function handle($request,Closure $next)
   {
       $authorization=function($ciphertext,$plaintext) {
+        if(empty($ciphertext) or empty($plaintext)){
+          return false;
+        }
         return substr($ciphertext,6)==base64_encode($plaintext);
       };
 
-      $auth= $authorization($request->header('authorization'),env('BASiC_HTTP_AUTH'));
+      $auth= $authorization($request->header('authorization'),env('HTTP_BASIC_AUTH'));
   
       if(!$auth){
-          $content=['msg'=>'error','dataInfo'=>new \stdClass];
+          $content=['msg'=>'error','data'=>['datainfo'=>new \stdClass]];
           return response(json_encode($content),401)->header('Content-type','application/json');
         };
 
